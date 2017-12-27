34c0a52b02f46a1a126d17bf0258e6d4300683bb
Skyler Hamilton <tycoonist925@gmail.com>
2017-12-27 01:27:43 +0000

    Forgot to update index.js, removed test out.jpg file, renamed helper notes thing

--- index.js
+++ index.js
@@ -1,24 +1,117 @@
 const piexif = require("piexifjs"),
   fs = require("fs-extra"),
-  im = require("imagemagick");
+  im = require("imagemagick"),
+  klaw = require('klaw'),
+  path = require('path'),
+  c = require('chalk'),
+  supportedExtensions = [".jpg",".jpeg",".tiff"];
 
-var inImage = "C:\\Users\\skyler.hamilton\\Pictures\\20170605_112040.jpg";
-var outImage = "out.jpg";
-
-var mapping = {
-  tags: {sect1: "0th", sect2: "40094", format: "int"},
-  author: {sect1: "0th", sect2: "315", format: "string"},
-  comment: {sect1: "0th", sect2: "40092", format: "int"},
-  title: {sect1: "0th", sect2: "270", format: "string"}
-}
-fs.readFile(inImage,(err,image)=>{
-  if (err) throw err;
-  else {
-    var data = image.toString("binary");
-    var exifObj = piexif.load(data);
-    //console.log(`data: ${JSON.stringify(exifObj)}`);
+const dataThatNeedsToBeDecoupled = {
+  inDir: "C:\\Data\\exif_editor_test_dir",
+  mapping: {
+    zeroth: [
+      {
+        name: "author",
+        value: "Author Value"
+      },
+      {
+        name: "title",
+        value: "Title Value"
+      },
+      {
+        name: "subject",
+        value: 'Subject Value'
+      }
+    ],
+    exif: []
   }
-});
+}
+init();
+function init() {
+  updateAllFiles();
+}
+
+function updateAllFiles() {
+  // get all matching files in directory first
+  let matchingFiles = [],invalidFiles=0;
+  klaw(dataThatNeedsToBeDecoupled.inDir)
+    .on('data', (item) => {
+      let obj = path.parse(item.path);
+      if (~supportedExtensions.indexOf(obj.ext)) matchingFiles.push(item.path);
+      else invalidFiles++;
+    })
+    .on('end', () => {
+      if (matchingFiles.length > 0) {
+        console.log(`${matchingFiles.length} out of ${matchingFiles.length+invalidFiles} will be updated`);
+        updateExif(0);
+      } else {
+        console.log(`0 out of ${invalidFiles} can be updated`);
+      }
+    })
+    function updateExif(index) {
+      // update the exif
+      if (index < matchingFiles.length) {
+        try {
+          fs.readFile(matchingFiles[index],(err,image) =>{
+            if (err) {
+                console.log(`${c.yellow("error reading image "+matchingFiles[index])}${c.red.bold(err)}`);
+                updateExif(index+1);
+            } else {
+              var data = image.toString("binary");
+              var exifObj = piexif.load(data);
+
+              var zeroth = {};
+              var exif = {};
+              var gps = {};
+              // we only update zeroth at the moment
+              for (let i=0;i<dataThatNeedsToBeDecoupled.mapping.zeroth.length;i++) {
+                switch(dataThatNeedsToBeDecoupled.mapping.zeroth[i].name) {
+                  case "author":
+                    zeroth[piexif.ImageIFD.Artist] = dataThatNeedsToBeDecoupled.mapping.zeroth[i].value;
+                    zeroth[piexif.ImageIFD.XPAuthor] = generateCharCodeArray(dataThatNeedsToBeDecoupled.mapping.zeroth[i].value);
+                    break;
+                  case "subject":
+                    zeroth[piexif.ImageIFD.XPSubject] = generateCharCodeArray(dataThatNeedsToBeDecoupled.mapping.zeroth[i].value);
+                    break;
+                  case "title":
+                    zeroth[piexif.ImageIFD.ImageDescription] = dataThatNeedsToBeDecoupled.mapping.zeroth[i].value;
+                    zeroth[piexif.ImageIFD.XPTitle] = generateCharCodeArray(dataThatNeedsToBeDecoupled.mapping.zeroth[i].value);
+                    break;
+                  case "tags":
+                    zeroth[piexif.ImageIFD.XPKeywords] = generateCharCodeArray(dataThatNeedsToBeDecoupled.mapping.zeroth[i].value);
+                    break;
+                  default:
+                    console.log(c.yellow(`property ${dataThatNeedsToBeDecoupled.mapping.zeroth[i].name} not yet supported`))
+                    break;
+                }
+              }
+              // time to save
+              var exifObj = {"0th":zeroth, "Exif":exif, "GPS":gps};
+              var exifbytes = piexif.dump(exifObj);
+
+              var newData = piexif.insert(exifbytes, data);
+              var newJpeg = new Buffer(newData, "binary");
+              fs.writeFile(matchingFiles[index], newJpeg, (err) => {
+                if (err) {
+                    console.log(`${c.yellow("error writing image "+matchingFiles[index])}${c.red.bold(err)}`);
+                } else {
+                  console.log(`updated file ${matchingFiles[index]}`)
+                }
+                updateExif(index+1);
+              });
+            }
+          })
+        } catch(err) {
+          console.log(`${c.yellow("error updating "+matchingFiles[index])}${c.red.bold(err)}`);
+          process.exit(0); // TODO: remove this line after successful runs
+          updateExif(index+1);
+        }
+      } else {
+        // done deal
+        console.log(`completed task I think`)
+      }
+    }
+}
 
 function generateCharCodeArray(string) {
   // add a 0 after ever char
